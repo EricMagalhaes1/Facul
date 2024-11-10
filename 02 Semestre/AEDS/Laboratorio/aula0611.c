019e29ad5943d674c612272e8b9632226bfb0396
Eric Magalhães <kir3vv@gmail.com>
2024-11-10 12:12:28 -0300

    Upload

---
+++ 02 Semestre/AEDS/Laboratorio/aula0611.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Produto{
+  int id;
+  char nome[50];
+  float preco;
+  int quantidade;
+}Produto;
+
+Produto cadastrarProd(){
+
+  Produto p;
+  getchar();
+  printf("Digite o nome do produto: ");
+  fgets(p.nome,sizeof(p.nome), stdin);
+  printf("Digite o ID:");
+  scanf("%d" , &p.id);
+  printf("Digite o valor do produto: ");
+  scanf("%f" , &p.preco);
+  printf("Digite a quantidade de produtos: ");
+  scanf("%f" , &p.quantidade);
+  printf("\n");
+
+  return p;
+}
+
+float precoMedio(Produto p[], int n){
+  float soma = 0;
+
+  for(int i = 0; i < n;i++){
+    soma += p[i].preco;
+  }
+
+  return soma/n;
+}
+
+void registrarVenda(char * pVenda, Produto * p, int n){
+
+  for(int i = 0; i < n; i++){
+    if((strcmp(p[i].nome, pVenda) && p[i].quantidade > 0) == 0){
+      p[i].quantidade --;
+      return;
+    }
+    printf("Venda nâo completada!\ng")
+  }
+}
+
+int main(){
+  int a;
+  Produto p[3];
+  
+  printf("Digite quantos produtos deseja insetir: ");
+  scanf("%d", &a);
+
+  for(int i = 0; i < a; i++){
+    p[i] = cadastrarProd();
+  }
+
+  float media = precoMedio(p, a);
+  printf("Preco medio: R$%.2f", media);
+
+  return 0;
+}
